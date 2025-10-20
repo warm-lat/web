62c378b3aa33fe0547c154757844e97b3bc0f9f2
nxyy <nxyy@nxyy.store>
2025-10-20 10:53:45 -0400

    g

--- src/app/(routes)/api/listen/search/route.ts
+++ src/app/(routes)/api/listen/search/route.ts
@@ -1,9 +1,18 @@
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/auth"
+
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const query = searchParams.get('query')
     
+    const session = await getServerSession(authOptions)
+    
+    if (!session?.user) {
+        return Response.json({ error: "Unauthorized" }, { status: 401 })
+    }
+
     const response = await fetch(
-        `https://listen.squareweb.app/search?query=${query}&key=${process.env.LISTEN_API_KEY}&userId=930383131863842816`
+        `https://listen.squareweb.app/search?query=${query}&key=${process.env.LISTEN_API_KEY}&userId=${session.user.id}`
     )
     const data = await response.json()
     return Response.json(data)
